d4fc941f4eb8b96153333041e9f200ca5629d133
benajakuhn <benaja.kuhn@students.fhnw.ch>
2025-03-30 18:07:49 +0200

    added possible representation for a game field

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Create an 8x8x3 NumPy array filled with False (boolean type)
+game_field_np = np.zeros((8, 8, 3), dtype=bool)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Entire game field as a 3D array:")
+print(game_field_np)
