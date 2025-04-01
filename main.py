3db0c4faba2698d98903f9af18dfcee64e4c812c
benajakuhn <benaja.kuhn@students.fhnw.ch>
2025-04-01 18:17:48 +0200

    switched from python to java

--- main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Create an 8x8x3 NumPy array filled with False (boolean type)
-game_field_np = np.zeros((8, 8, 3), dtype=bool)
-
-print("Entire game field as a 3D array:")
-print(game_field_np)
