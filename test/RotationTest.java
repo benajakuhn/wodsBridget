74ad33afdb348697b1e4aa21cf5421c6000fb8e1
benajakuhn <benaja.kuhn@students.fhnw.ch>
2025-04-07 16:57:37 +0200

    added tests for all the blocks -> Z 24 and O 12 are to many but i don't know how to catch it because they are moved.

--- test/RotationTest.java
+++ test/RotationTest.java
@@ -8,15 +8,27 @@ public class RotationTest {
     // Test T-block rotations
     @Test
     public void testTBlockRotation() {
-        BlockRotator3D.placeAllUniqueRotations(Main.tBlockShape, 3,3,1, BlockRotator3D.TBLOCK_ROTATION_INDICES);
+        BlockRotator3D.placeAllUniqueRotations(Main.tBlockShape,  BlockRotator3D.TBLOCK_ROTATION_INDICES);
         assertEquals(12, BlockRotator3D.TBLOCK_ROTATION_INDICES.length);
     }
 
     // Test L-Block rotations
     @Test
     public void testLBlockRotation() {
-        BlockRotator3D.placeAllUniqueRotations(Main.lBlockShape, 3,3,1, BlockRotator3D.LBLOCK_ROTATION_INDICES);
+        BlockRotator3D.placeAllUniqueRotations(Main.lBlockShape,  BlockRotator3D.LBLOCK_ROTATION_INDICES);
         assertEquals(24, BlockRotator3D.LBLOCK_ROTATION_INDICES.length);
+    }
+
+    @Test
+    public void testZBlockRotation() {
+        BlockRotator3D.placeAllUniqueRotations(Main.zBlockShape,  BlockRotator3D.ZBLOCK_ROTATION_INDICES);
+        assertEquals(24, BlockRotator3D.ZBLOCK_ROTATION_INDICES.length);
+    }
+
+    @Test
+    public void testOBlockRotation() {
+        BlockRotator3D.placeAllUniqueRotations(Main.oBlockShape,  BlockRotator3D.OBLOCK_ROTATION_INDICES);
+        assertEquals(12, BlockRotator3D.OBLOCK_ROTATION_INDICES.length);
     }
 
 
