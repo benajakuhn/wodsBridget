891c0c09b413b74add646acc58ddf78ca516abd9
benajakuhn <benaja.kuhn@students.fhnw.ch>
2025-05-27 15:53:20 +0200

    added getMoveByDepth and changed form random move to moves generated with minimax

--- src/Profile.java
+++ src/Profile.java
@@ -1,6 +1,7 @@
 public class Profile {
     public static void main(String[] args) {
         initializeBoardWithRandomMoves(5);
+        getMoveByDepth(3);
     }
 
     // init the board with random moves for both players
@@ -9,14 +10,14 @@ public class Profile {
         Main.clearGameBoard();
         Move move;
         for (int i = 0; i < movesPerPlayer; i++) {
-            move = Main.generateRandomMove(1);
+            move = Minimax_AlphaBeta.findBestMove(1);
             if(BlockRotator3D.placeShape(move.getTransformedShape(), move.x, move.y, 1)){
                 System.out.println("Player 1 placed: " + move);
                 Minimax_AlphaBeta.player1Inventory.usePiece(Main.getPieceType(move.shape));
                 Minimax_History.player1Inventory.usePiece(Main.getPieceType(move.shape));
                 MTDf.player2Inventory.usePiece(Main.getPieceType(move.shape));
             }
-            move = Main.generateRandomMove(2);
+            move = Minimax_AlphaBeta.findBestMove(2);
             if(BlockRotator3D.placeShape(move.getTransformedShape(), move.x, move.y, 2)){
                 System.out.println("Player 2 placed: " + move);
                 Minimax_AlphaBeta.player2Inventory.usePiece(Main.getPieceType(move.shape));
@@ -41,5 +42,17 @@ public class Profile {
             ", L: " + Minimax_History.player2Inventory.getPieceCount("L") +
             ", Z: " + Minimax_History.player2Inventory.getPieceCount("Z") +
             ", O: " + Minimax_History.player2Inventory.getPieceCount("O"));
+    }
+    public static void getMoveByDepth(int depth) {
+        System.out.println("\nStarting Minimax AlphaBeta search with depth: " + depth);
+        Minimax_AlphaBeta.MAX_DEPTH = depth;
+        Minimax_AlphaBeta.findBestMove(1);
+
+        System.out.println("\nStarting Minimax History search with depth: " + depth);
+        Minimax_History.MAX_DEPTH = depth;
+        Minimax_History.findBestMove(Integer.MAX_VALUE);
+
+        System.out.println("\nStarting MTD-f search with depth: " + depth);
+        MTDf.findBestMoveMTDf(depth, Integer.MAX_VALUE, 0, 1);
     }
 }
